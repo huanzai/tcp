632c34fb1670b2adbf8890e822f0ef767ac337b3
liuhuan <liuhuan@ejoy.com>
2016-10-09 18:29:27 +0800

    second version

--- tcp.h
+++ tcp.h
@@ -10,6 +10,8 @@ typedef int (*opfunc)(const char*, int);
 struct tcp_state* tcp_create();
 void tcp_release(struct tcp_state* T);
 
+void tcp_update(struct tcp_state* T, uint32_t current);
+
 int tcp_send(struct tcp_state* T, const char* buffer, int len);
 int tcp_recv(struct tcp_state* T, char* buffer, int len);
 
